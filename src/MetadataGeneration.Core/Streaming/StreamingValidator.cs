7b2ce30caf888c4cacee957c5ce1de2f9627d5e0
bitpusher <sky.sanders@gmail.com>
2011-11-30 08:11:21 -0700

    validate rpc service parameters and streaming return types

---
+++ src/MetadataGeneration.Core/Streaming/StreamingValidator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MetadataGeneration.Core.JsonSchemaDTO;
+using Newtonsoft.Json.Linq;
+
+namespace MetadataGeneration.Core.Streaming
+{
+    public class StreamingValidator
+    {
+        public void ValidateStreamingFragment(JObject ssmd, JObject schema)
+        {
+            var ex = new MetadataValidationException(typeof(object), "streaming","streaming validation failed","see AggregatedExceptions");
+
+
+            IEnumerable<JProperty> services = ((JObject)ssmd["services"]).Properties();
+            foreach (JProperty service in services)
+            {
+                JToken svc = service.Value;
+                var returnValue = svc["returns"]["$ref"].Value<string>();
+                if (returnValue.StartsWith("#."))
+                {
+                    returnValue = returnValue.Substring(2);
+                }
+                if (schema["properties"][returnValue] == null)
+                {
+                    var method = svc["target"].Value<string>()+"."+ svc["channel"].Value<string>();
+                    ex.AggregatedExceptions.Add(new MetadataValidationException(typeof(Object), method, "Every service return type must be represented in the json-schema.", "ensure that the return type is represented in the json schema"));
+                  
+                }
+            }
+
+            if(ex.AggregatedExceptions.Count>0)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
